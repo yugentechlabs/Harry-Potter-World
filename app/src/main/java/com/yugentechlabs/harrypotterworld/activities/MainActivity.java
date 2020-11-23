a63ee08474c3c4650147dafad6e8ea2ac6b93b92
Anusha <anusha267@gmail.com>
2020-11-23 19:09:12 +0530

    23/11

--- app/src/main/java/com/yugentechlabs/harrypotterworld/activities/MainActivity.java
+++ app/src/main/java/com/yugentechlabs/harrypotterworld/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.yugentechlabs.harrypotterworld;
+package com.yugentechlabs.harrypotterworld.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.yugentechlabs.harrypotterworld.R;
 
 public class MainActivity extends AppCompatActivity {
 
